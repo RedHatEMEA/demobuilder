40dbd601fe223c41277a05f978916b178e6e6dbe
Jim Minter <jminter@redhat.com>
2014-09-17 15:51:09 +0100

    more cleanup fixes

--- build.sh
+++ build.sh
@@ -13,21 +13,25 @@ build() {
   done
 }
 
-cleanup() {
-  proxy_stop
-  httpserver_stop
-}
-
 utils/init.sh
 
-trap cleanup 0
 proxy_start
 httpserver_start
 
 if [ $# -eq 0 ]; then
+  echo "Usage: $0 -a"
+  echo "       $0 target"
+  echo
+  echo "Valid targets:"
+  ls targets | sed -e 's/^/  /'
+  echo
+elif [ $1 = "-a" ]; then
   for i in targets/*; do
     build $(basename $i)
   done
 else
   build $1
 fi
+
+proxy_stop
+httpserver_stop
