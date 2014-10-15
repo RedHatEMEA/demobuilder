fbacad94f5efb4801547153b81d6df457402f55e
Jim Minter <jminter@redhat.com>
2014-10-15 07:02:01 +0100

    RHEL 7 kickstart, thanks jread

--- utils/wait-ip.py
+++ utils/wait-ip.py
@@ -1,81 +1,79 @@
 #!/usr/bin/python
 
-import asyncore
 import json
+import Queue
 import socket
 import sys
+import threading
 import time
 
 
-class VirtioDispatcher(asyncore.dispatcher):
-    def __init__(self, sock):
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        while True:
-            try:
-                self.connect(sock)
-                break
-
-            except socket.error:
-                time.sleep(1)
-        self.buf = ""
-
-    def writable(self):
-        return (len(self.buf) > 0)
-
-    def handle_write(self):
-        sent = self.send(self.buf)
-        self.buf = self.buf[sent:]
-
-
-class RHEVAgent(VirtioDispatcher):
-    def handle_read(self):
-        global ip
-
-        r = self.recv(4096)
-        if not r: return
-
-        r = json.loads(r)
-        if r["__name__"] == "network-interfaces":
-            ip = r["interfaces"][0]["inet"][0]
-            asyncore.close_all()
-
-
-class QEmuAgent(VirtioDispatcher):
-    def __init__(self, sock):
-        VirtioDispatcher.__init__(self, sock)
-        self.buf = json.dumps({"execute": "guest-network-get-interfaces"})
-
-    def handle_read(self):
-        global ip
-
-        r = self.recv(4096)
-        if not r: return
-
-        r = json.loads(r)
-        eth = [eth for eth in r["return"] if eth["name"] == "eth0"][0]
-        ip = [ip for ip in eth["ip-addresses"]
-              if ip["ip-address-type"] == "ipv4"][0]
-
-        ip = ip["ip-address"]
-        asyncore.close_all()
-
-
-def main():
-    rhev = RHEVAgent("%s/rhev.sock" % sys.argv[1])
-    qemu = QEmuAgent("%s/qemu.sock" % sys.argv[1])
-    asyncore.loop()
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def connect(t, p):
+    s = socket.socket(t, socket.SOCK_STREAM)
     while True:
         try:
-            sock.connect((ip, 22))
-            break
+            s.connect(p)
+            return s
 
         except socket.error:
             time.sleep(1)
 
+
+def start_thread(target, arg):
+    t = threading.Thread(target=target, args=(arg, ))
+    t.daemon = True
+    t.start()
+    return t
+
+
+def rhev_thread(p):
+    s = connect(socket.AF_UNIX, p)
+
+    while True:
+        r = json.loads(s.recv(4096))
+
+        if r["__name__"] == "network-interfaces":
+            q.put(r["interfaces"][0]["inet"][0])
+            return
+
+
+def qemu_thread(p):
+    s = connect(socket.AF_UNIX, p)
+
+    while True:
+        try:
+            s.sendall(json.dumps({"execute": "guest-network-get-interfaces"}))
+            r = json.loads(s.recv(4096))
+
+            eth = [eth for eth in r["return"] if eth["name"] == "eth0"][0]
+            ip = [ip for ip in eth["ip-addresses"]
+                  if ip["ip-address-type"] == "ipv4"][0]
+
+            q.put(ip["ip-address"])
+            return
+
+        except (IndexError, KeyError):
+            time.sleep(1)
+
+
+def main():
+    global q
+    q = Queue.Queue()
+
+    start_thread(rhev_thread, "%s/rhev.sock" % sys.argv[1])
+    start_thread(qemu_thread, "%s/qemu.sock" % sys.argv[1])
+
+    while True:
+        try:
+            ip = q.get(False)
+            break
+
+        except Queue.Empty:
+            time.sleep(1)
+
+    connect(socket.AF_INET, (ip, 22))
     print "IP=%s" % ip
+
 
 if __name__ == "__main__":
     main()
