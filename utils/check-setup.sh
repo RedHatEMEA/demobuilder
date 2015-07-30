97fa8b7d281ce233839042300186f380bd4f1ade
Jim Minter <jminter@redhat.com>
2015-07-30 17:09:25 +0100

    add check-setup script

---
+++ utils/check-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+. utils/functions
+
+for pkg in git libcdio libguestfs libvirt pigz pyOpenSSL python-bottle qemu-kvm; do
+  rpm -q $pkg &>/dev/null || echo "Please install $pkg."
+done
+
+for proc in libvirtd; do
+  pidof $proc &>/dev/null || echo "Please start $proc."
+done
+
+sudo iptables -C INPUT_ZONES -i $BRIDGE -j IN_trusted &>/dev/null || echo "Please verify firewall configuration."
