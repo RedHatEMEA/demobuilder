8a034a36930d0fc51bd3435857714ade5f58305c
Jim Minter <jminter@redhat.com>
2015-07-30 14:37:59 +0100

    add releases capability

--- ui/app.py
+++ ui/app.py
@@ -4,28 +4,44 @@ from bottle import *
 import glob
 import os
 
-@route("/")
-@view("ui/index.html.tpl")
-def root():
-    targets = [os.path.basename(x[:-1]) for x in glob.glob("build/*/")]
-    images = {os.path.basename(x).rsplit(".")[0]: "" for x in glob.glob("build/*/*")}
 
+def make_table(targets, images, root):
     table = []
     for i in images:
         row = [i]
         for t in targets:
-            x = glob.glob("build/%s/%s.*" % (t, i))
+            x = glob.glob("%s/%s/%s.*" % (root, t, i))
             if x:
                 row.append(x[0])
             else:
                 row.append(None)
         table.append(row)
 
-    return {"targets": targets, "table": table}
+    return table
+
+
+@route("/")
+@view("ui/index.html.tpl")
+def root():
+    targets = {os.path.basename(x[:-1])
+               for x in glob.glob("build/*/") + glob.glob("releases/*/")}
+
+    build = {os.path.basename(x).rsplit(".")[0]: ""
+              for x in glob.glob("build/*/*")}
+    build_table = make_table(targets, build, "build")
+
+    releases = {os.path.basename(x).rsplit(".")[0]: ""
+              for x in glob.glob("releases/*/*")}
+    releases_table = make_table(targets, releases, "releases")
+
+    return {"targets": targets, "build_table": build_table,
+            "releases_table": releases_table}
+
 
 @route("/<path:path>")
 def download(path):
     return static_file(path, ".")
+
 
 if __name__ == "__main__":
     run(host="0.0.0.0", port=80)
--- ui/index.html.tpl
+++ ui/index.html.tpl
@@ -9,7 +9,8 @@
 <body>
   <h1>Welcome to {{ socket.gethostname() }}!</h1>
 
-  <h2>Available images</h2>
+  % if releases_table:
+  <h2>Release images</h2>
   <ul>
     <table>
       <tr>
@@ -18,7 +19,7 @@
         <th valign="bottom" width="150">{{ target }}</th>
         % end
       </tr>
-      % for row in table:
+      % for row in releases_table:
       <tr>
         <th align="right">{{ row[0] }}</th>
         % for item in row[1:]:
@@ -32,7 +33,34 @@
       % end
     </table>
   </ul>
+  %end
 
+  % if build_table:
+  <h2>Development images</h2>
+  <ul>
+    <table>
+      <tr>
+        <th></th>
+        % for target in targets:
+        <th valign="bottom" width="150">{{ target }}</th>
+        % end
+      </tr>
+      % for row in build_table:
+      <tr>
+        <th align="right">{{ row[0] }}</th>
+        % for item in row[1:]:
+        <td align="center">
+          % if item:
+          <a href="{{ item }}">download</a>
+          % end
+        </td>
+        % end
+      </tr>
+      % end
+    </table>
+  </ul>
+  %end
+  
   <h2>Give feedback</h2>
   <ul>
     Questions, comments, feature requests, patches, pull requests and cake are all welcomed at <a href="mailto:jminter@redhat.com">jminter@redhat.com</a>.
