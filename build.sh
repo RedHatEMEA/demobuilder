1de6de92420d44934ec0a41179def0b452b10355
Jim Minter <jminter@redhat.com>
2015-08-10 12:50:02 +0100

    usability

--- build.sh
+++ build.sh
@@ -2,33 +2,7 @@
 
 . utils/functions
 
-start() {
-  apiserver_start
-}
-
-stop() {
-  apiserver_stop
-}
-
-install_layer() {
-  echo "Building $1..."
-
-  if [ -d layers/$1 ]; then
-    if [ -x layers/$1/install ]; then
-      layers/$1/install
-    else
-      utils/install-layer.sh $1
-    fi
-
-  else
-    targets/${1##*:}/install ${1%:*}
-  fi
-}
-
 utils/init.sh
-
-trap stop EXIT
-start
 
 if [ $# -eq 0 ]; then
   echo "Usage: $0 layer[:target]"
@@ -41,7 +15,21 @@ if [ $# -eq 0 ]; then
   echo
 
 else
-  install_layer $1
-fi
+  trap apiserver_stop EXIT
+  apiserver_start
 
-echo "$0: Done."
+  echo "Building $1..."
+
+  if [ -d layers/$1 ]; then
+    if [ -x layers/$1/install ]; then
+      layers/$1/install
+    else
+      utils/install-layer.sh $1
+    fi
+
+  else
+    targets/${1##*:}/install ${1%:*}
+  fi
+
+  echo "$0: Done."
+fi
--- utils/init.sh
+++ utils/init.sh
@@ -2,12 +2,36 @@
 
 . utils/functions
 
+FATAL=0
+
 for pkg in git libcdio libguestfs libvirt pigz pyOpenSSL python-bottle PyYAML qemu-kvm; do
-  rpm -q $pkg &>/dev/null || echo "WARNING: please install $pkg."
+  if ! rpm -q $pkg &>/dev/null; then
+    echo "FATAL: please install $pkg.  You probably need to run:"
+    echo "sudo yum -y install $pkg"
+    echo
+    FATAL=1
+  fi
 done
 
-for proc in libvirtd; do
-  pidof $proc &>/dev/null || echo "WARNING: please start $proc."
+for svc in libvirtd; do
+  if ! pidof $svc &>/dev/null; then
+    echo "FATAL: please start $svc.  You probably need to run:"
+    echo "sudo systemctl enable $svc.service"
+    echo "sudo systemctl start $svc.service"
+    echo
+    FATAL=1
+  fi
+done
+
+for contrib in contrib/*; do
+  if [ ! -e $contrib/.git ]; then
+    echo "FATAL: please check submodules out correctly.  In the future, use the -u option"
+    echo "       to git clone.  For now, you probably need to run:"
+    echo "git submodule init $contrib"
+    echo "git submodule update $contrib"
+    echo
+    FATAL=1
+  fi
 done
 
 sudo iptables -C INPUT_ZONES -i $BUILD_BRIDGE -j IN_trusted &>/dev/null || echo "WARNING: please verify firewall configuration."
@@ -20,4 +44,8 @@ fi
 
 if [ ! -e config.yml ]; then
   cp config.yml.example config.yml
+fi
+
+if [ $FATAL != 0 ]; then
+  exit 1
 fi
