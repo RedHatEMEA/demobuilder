2cf3b9b6c84e15a64e71996bba36ac63fb3ca235
Jim Minter <jminter@redhat.com>
2015-08-28 15:29:11 +0100

    add timeout to dnslib to avoid hang

--- layers/rhel-server-7:gui:ose-3.0/@target/openshift-dns-intercept.py
+++ layers/rhel-server-7:gui:ose-3.0/@target/openshift-dns-intercept.py
@@ -9,6 +9,17 @@ import socket
 import sys
 
 
+class TimeoutInterceptResolver(dnslib.intercept.InterceptResolver):
+    def resolve(self, request, handler):
+        old_send = request.send
+        def new_send(self, *k, **kw):
+            kw["timeout"] = 1
+            return old_send(self, *k, **kw)
+        request.send = new_send
+
+        return super(TimeoutInterceptResolver, self).resolve(request, handler)
+
+
 def handler(signum, frame):
     os.system("iptables -w -t nat -D PREROUTING -i tun0 -d %s -p udp --dport 53 -j REDIRECT --to-port 153" % NSIP)
     sys.exit(0)
@@ -25,6 +36,6 @@ NSIP = m.group(1)
 
 os.system("iptables -w -t nat -I PREROUTING -i tun0 -d %s -p udp --dport 53 -j REDIRECT --to-port 153" % NSIP)
 
-resolver = dnslib.intercept.InterceptResolver(NSIP, 53, "60s", ["%s 60 IN A %s" % (HOSTNAME, IP)], [], [])
+resolver = TimeoutInterceptResolver(NSIP, 53, "60s", ["%s 60 IN A %s" % (HOSTNAME, IP)], [], [])
 udp_server = dnslib.server.DNSServer(resolver, port=153)
 udp_server.start()
