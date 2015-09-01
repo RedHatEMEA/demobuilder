cf28aaefa62aec107628d4d7da233b30a54064b2
Jim Minter <jminter@redhat.com>
2015-09-01 12:29:28 +0100

    pragma

--- utils/clean-repodata.sh
+++ utils/clean-repodata.sh
@@ -2,6 +2,6 @@
 
 . utils/functions
 
-sqlite3 webproxycache.db 'DELETE FROM cache WHERE url LIKE "%/repodata/%";'
-sqlite3 webproxycache.db 'DELETE FROM cache WHERE url LIKE "https://registry-1.docker.io:443/v2/%/manifests/%";'
-sqlite3 webproxycache.db 'DELETE FROM cache WHERE url LIKE "https://registry.access.redhat.com:443/v1/repositories/%";'
+sqlite3 webproxycache.db 'PRAGMA foreign_keys = ON; DELETE FROM cache WHERE url LIKE "%/repodata/%";'
+sqlite3 webproxycache.db 'PRAGMA foreign_keys = ON; DELETE FROM cache WHERE url LIKE "https://registry-1.docker.io:443/v2/%/manifests/%";'
+sqlite3 webproxycache.db 'PRAGMA foreign_keys = ON; DELETE FROM cache WHERE url LIKE "https://registry.access.redhat.com:443/v1/repositories/%";'
