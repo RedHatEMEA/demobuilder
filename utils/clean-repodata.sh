05664d3b2615177b4df40130d5f81f6df36f2d57
Jim Minter <jminter@redhat.com>
2015-09-03 16:03:38 +0100

    fix up maven usage

--- utils/clean-repodata.sh
+++ utils/clean-repodata.sh
@@ -5,3 +5,4 @@
 sqlite3 webproxycache.db 'PRAGMA foreign_keys = ON; DELETE FROM cache WHERE url LIKE "%/repodata/%";'
 sqlite3 webproxycache.db 'PRAGMA foreign_keys = ON; DELETE FROM cache WHERE url LIKE "https://registry-1.docker.io:443/v2/%/manifests/%";'
 sqlite3 webproxycache.db 'PRAGMA foreign_keys = ON; DELETE FROM cache WHERE url LIKE "https://registry.access.redhat.com:443/v1/repositories/%";'
+sqlite3 webproxycache.db 'PRAGMA foreign_keys = ON; DELETE FROM cache WHERE url LIKE "https://auth.docker.io:443/%";'
