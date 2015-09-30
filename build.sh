85cbe82d85b0758fc8ff1d6d839efa91acfcf3e7
jonmwalton <jwalton@redhat.com>
2015-09-30 15:49:34 +0100

    Recursive build from one command

--- build.sh
+++ build.sh
@@ -17,20 +17,40 @@ if [ $# -eq 0 ]; then
 else
   trap apiserver_stop EXIT
   apiserver_start
-
+  
   for item in "$@"; do
-    echo "Building $item..."
-
-    if [ -d layers/$item ]; then
-      if [ -x layers/$item/install ]; then
-        layers/$item/install
+    echo "Building layers $item..."
+    layers=$item
+    parentLayer=${layers%%:*}
+    continue=true
+    
+    while [ continue ]; do 
+      echo "Checking $parentLayer exists..."     
+      if ! [[ -a build/$parentLayer.qcow2 ]]; then
+        echo "Need to build $parentLayer first"
+          if [ -d layers/$parentLayer ]; then
+      if [ -x layers/$parentLayer/install ]; then
+        layers/$parentLayer/install
       else
-        utils/install-layer.sh $item
+        utils/install-layer.sh $parentLayer
       fi
-
     else
-      targets/${item##*:}/install ${item%:*}
+      targets/${parentLayer##*:}/install ${parentLayer%:*}
     fi
+      else
+        echo "$parentLayer already built, moving onto next"
+      fi      
+      
+      if [[ $layers == *":"* ]]; then
+        #echo "more layers to process"
+        layers=${item/$parentLayer:/""}
+        parentLayer=$parentLayer:${layers%%:*}
+      else
+        #echo "no more layers"
+        let continue=false
+      fi
+    done
+    
 
     echo "$0: Done building $item."
   done
