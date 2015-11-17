8744539c137b8750c9f9c91f7f84a659ef2c0253
Jim Minter <jminter@redhat.com>
2015-11-17 20:36:31 +0000

    fixes to recursive-build

--- build.sh
+++ build.sh
@@ -17,41 +17,30 @@ if [ $# -eq 0 ]; then
 else
   trap apiserver_stop EXIT
   apiserver_start
-  
-  for item in "$@"; do
-    echo "Building layers $item..."
-    layers=$item
-    parentLayer=${layers%%:*}
-    continue=true
-    
-    while [ continue ]; do 
-      echo "Checking $parentLayer exists..."     
-      if ! [[ -a build/$parentLayer.qcow2 ]]; then
-        echo "Need to build $parentLayer first"
-          if [ -d layers/$parentLayer ]; then
-      if [ -x layers/$parentLayer/install ]; then
-        layers/$parentLayer/install
-      else
-        utils/install-layer.sh $parentLayer
-      fi
-    else
-      targets/${parentLayer##*:}/install ${parentLayer%:*}
-    fi
-      else
-        echo "$parentLayer already built, moving onto next"
-      fi      
-      
-      if [[ $layers == *":"* ]]; then
-        #echo "more layers to process"
-        layers=${item/$parentLayer:/""}
-        parentLayer=$parentLayer:${layers%%:*}
-      else
-        #echo "no more layers"
-        let continue=false
-      fi
-    done
-    
 
-    echo "$0: Done building $item."
+  for item in "$@"; do
+    currentLayer=${item%%:*}
+    remainingLayers=${item#*:}
+
+    while true; do
+      echo "$0: Building $currentLayer..."
+      if [ -d layers/$currentLayer ]; then
+        if [ -x layers/$currentLayer/install ]; then
+          layers/$currentLayer/install
+        else
+          utils/install-layer.sh $currentLayer
+        fi
+      else
+        targets/${currentLayer##*:}/install ${currentLayer%:*}
+      fi
+      echo "$0: Done building $currentLayer."
+
+      if [[ "$currentLayer" == "$item" ]]; then
+        break
+      fi
+
+      currentLayer=$currentLayer:${remainingLayers%%:*}
+      remainingLayers=${remainingLayers#*:}
+    done
   done
 fi
--- utils/install-layer.sh
+++ utils/install-layer.sh
@@ -5,6 +5,10 @@
 LAYER=$1
 BASE=${LAYER%:*}
 
+if ! needs_build build/$LAYER.qcow2 layers/$LAYER build/$BASE.qcow2; then
+  exit 0
+fi
+
 utils/addlayer.sh build/$BASE.qcow2 layers/$LAYER tmp/$LAYER.qcow2
 
 mv tmp/$LAYER.qcow2 build/$LAYER.qcow2
