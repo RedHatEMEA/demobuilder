eeb7bbe68af9c65a6af760e847174eb243355407
Jim Minter <jminter@redhat.com>
2016-05-16 09:42:59 +0100

    update ci-build.sh

--- utils/ci-build.sh
+++ utils/ci-build.sh
@@ -3,12 +3,12 @@
 exec ./build.py \
   rhel-server-7:gui:fuse-6.2:vagrant-libvirt \
   rhel-server-7:gui:fuse-6.2:vagrant-virtualbox \
-  rhel-server-7:gui:ose-3.1:vagrant-libvirt \
-  rhel-server-7:gui:ose-3.1:vagrant-virtualbox \
-  rhel-server-7:gui:ose-3.1:offline:vagrant-libvirt \
-  rhel-server-7:gui:ose-3.1:offline:vagrant-virtualbox \
-  rhel-server-7:gui:ose-3.1:offline:demo:vagrant-libvirt \
-  rhel-server-7:gui:ose-3.1:offline:demo:vagrant-virtualbox \
-  rhel-server-7:gui:ose-3.1:offline:demo:aws \
+  rhel-server-7:gui:ose-3.2:vagrant-libvirt \
+  rhel-server-7:gui:ose-3.2:vagrant-virtualbox \
+  rhel-server-7:gui:ose-3.2:offline:vagrant-libvirt \
+  rhel-server-7:gui:ose-3.2:offline:vagrant-virtualbox \
+  rhel-server-7:gui:ose-3.2:offline:demo:vagrant-libvirt \
+  rhel-server-7:gui:ose-3.2:offline:demo:vagrant-virtualbox \
+  rhel-server-7:gui:ose-3.2:offline:demo:aws \
   rhel-server-7:gui:rhelosp-7:vagrant-libvirt \
   fedora-21-32:gui:webex:vagrant-libvirt
